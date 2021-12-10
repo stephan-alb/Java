910c7ad47995ad549f7d660b258f2d62fdfc5d59
stephan-alb <77104399+stephan-alb@users.noreply.github.com>
2021-12-10 13:07:51 +0100

    Add files via upload

---
+++ HalloWelt.java
@@ -0,0 +1,10 @@
+
+public class HalloWelt {
+
+	public static void main(String[] args) {
+		// TODO Automatisch generierter Methodenstub
+		System.out.println("Hallo Welt");
+		System.out.println("Es grüßt dich Stephan Albrecht");
+	}
+
+}
